35228ab66bf19b547cfe21a71f7d40687ba13660
Jamal Ahmed <j8ahmed@gmail.com>
2018-10-03 10:02:04 -0400

    adjustments to include css loader to webpack.config

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
 import '../App.css';
 
+
+
 class App extends Component {
     render() {
         return (
             <div>
-                <h1>My React Redux App</h1>
-                <h2>By: {this.props.title}</h2>
+               <h1>My React Redux App</h1>
+               <h3> By: {this.props.title}</h3>
+               
             </div>
         )
         
