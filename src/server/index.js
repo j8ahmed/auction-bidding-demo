ea2cc0c1f35a43012ee0f2aa89160123ba0065ba
Jamal Ahmed <j8ahmed@gmail.com>
2018-10-07 07:42:59 -0400

    updated the server

--- src/server/index.js
+++ src/server/index.js
@@ -9,7 +9,7 @@ let app = express();
 //create a server
 
 let server = app.listen(4000, () => {
-    console.log("listening to requests on port 4000");
+    console.log("listening to requests on port");
 });
 
 //Static Files
@@ -20,19 +20,16 @@ app.use(express.static('../'));
 let io = socket(server);
 
 io.on('connection', (socket) =>{
-    console.log("Made a socket connection \n", socket.id);
+    console.log(socket.id);
 
     //listen to bids from the bidder
     socket.on('bid', (data) => {
-        console.log(data.id);
         /*send the data sent from one client to all the other client connect through other sockets*/ 
         io.emit('bid', data);
-        console.log("sent info!!!");
     });
 
     //listen to the starting price from the Auction Clerk
     socket.on('auctionStart', (data) => {
         socket.broadcast.emit('auctionStart', data);
-        console.log('sent to all the sockets');
     });
 });
